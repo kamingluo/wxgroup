0e3d81b3a5bf756dc9c6fb19bc3c26a743092011
kaming <ljm@xlbzone.com>
2020-04-11 16:59:45 +0800

    开奖接口

--- php/application/miniapp/controller/group/Openlottery.php
+++ php/application/miniapp/controller/group/Openlottery.php
@@ -4,6 +4,8 @@ use think\Db;
 use think\Request;
 use think\Config;
 use processing\Lotterynum;
+use temmsg\Lotterymsg;
+use think\Log;
 
 class Openlottery
 {
@@ -16,19 +18,131 @@ class Openlottery
     {
         $lottery_id=$request->param("lottery_id");//抽奖id
         //先保证有用户参与才能开奖
-        $partakecount=db('lottery_partake_list')->where('lottery_id',$lottery_id)->count();
-        if($partakecount < 1){
-            $state=['state'   => '400','message'  => "无用户参与不能开奖" ];
-            return $state;
-        }
+
+        //计算中奖人数
+        $Lotterynum = new Lotterynum();
+        $opennumber=$Lotterynum -> opennumber($lottery_id);
+        // return $opennumber;
+
+        //先将全部人设置为未中奖和已经开奖
+        $dbreturn= db('lottery_partake_list')->where('lottery_id',$lottery_id)->update(['prize' => 1,'state' => 0]);
+
+        //再根据中奖人数，随机挑选设置为中奖
+        $sql="UPDATE lottery_partake_list SET prize=0 WHERE  lottery_id =".$lottery_id." and id in ( SELECT F.id FROM (SELECT * FROM lottery_partake_list ORDER BY RAND() LIMIT ".$opennumber.") F);";
+        // $Model = new Model();
+
+        $updatenum = Db::execute($sql); //执行，随机选取中奖人数
+        $updatestate= db('lottery_crowd_list')->where('id',$lottery_id)->update(['state' => 1]);//更新抽奖活动为已经，开奖
+
         $lottery_data=db('lottery_crowd_list')->where('id',$lottery_id)->find(); //拿到抽奖活动信息
-
-
+        $goods_name=$lottery_data['goods_name'];//拿到奖品名称
+        //然后发送消息给参与的人
+        $lotterymsg = new Lotterymsg();
+        $text = $lotterymsg -> lotterymssage($lottery_id,$goods_name);
        
+         return ['state'   => '200','message'  => "手动开奖成功" ];
 
-        return "手动开奖";
         
     }
+
+   
+    //自动开奖
+    public function automatic(){
+        $time =date('Y-m-d H:i:s',time());//获取当前时间
+
+        //查询要开奖的数据
+
+        $lotterydata =db('lottery_crowd_list')->where('luck_mode',1)->where('state',0)->whereTime('time_lottery', '<=', $time)->select();//根据抽奖id拿到要推送的用户
+        $count = count($lotterydata);//拿到数值条数
+        $Lotterynum = new Lotterynum();//计算开奖人数
+        $lotterymsg = new Lotterymsg();//发送开奖通知
+
+        foreach($lotterydata as $count  => $data){
+            $lottery_id=$data["id"];//抽奖活动id
+            $partakecount=db('lottery_partake_list')->where('lottery_id',$lottery_id)->count();//拿到抽奖活动参与人数
+            if($partakecount < 1){
+                Log::record("不开奖");
+                //无用户参与，不用开奖，直接把活动设置已经开奖
+                $updatestate= db('lottery_crowd_list')->where('id',$lottery_id)->update(['state' => 1]);//更新抽奖活动为已经，开奖
+            }
+            else{
+                //有用户参与，开奖
+                Log::record("执行开奖");
+                $opennumber=$Lotterynum -> opennumber($lottery_id);
+
+                //先将全部人设置为未中奖和已经开奖
+                $dbreturn= db('lottery_partake_list')->where('lottery_id',$lottery_id)->update(['prize' => 1,'state' => 0]);
+
+                //再根据中奖人数，随机挑选设置为中奖
+                $sql="UPDATE lottery_partake_list SET prize=0 WHERE  lottery_id =".$lottery_id." and id in ( SELECT F.id FROM (SELECT * FROM lottery_partake_list ORDER BY RAND() LIMIT ".$opennumber.") F);";
+                // $Model = new Model();
+
+                $updatenum = Db::execute($sql); //执行，随机选取中奖人数
+                $updatestate= db('lottery_crowd_list')->where('id',$lottery_id)->update(['state' => 1]);//更新抽奖活动为已经，开奖
+
+                $lottery_data=db('lottery_crowd_list')->where('id',$lottery_id)->find(); //拿到抽奖活动信息
+                $goods_name=$lottery_data['goods_name'];//拿到奖品名称
+                //然后发送消息给参与的人
+                $text = $lotterymsg -> lotterymssage($lottery_id,$goods_name);
+            }
+
+
+        }
+
+        return "自动开奖完成";
+
+    }
+
+
+
+   public function fullnumber(Request $request)
+    {
+
+        $lottery_id=$request->param("lottery_id");//抽奖id
+        $lottery_data=db('lottery_crowd_list')->where('id',$lottery_id)->find(); //拿到抽奖活动信息
+
+        $state=$lottery_data['state'];//是否已经开奖
+        $luck_mode=$lottery_data['luck_mode'];//开奖方式0是满人开奖，1是到时间开奖
+        $packed_lottery=$lottery_data['packed_lottery'];//满多少人开奖数
+        $partakecount=db('lottery_partake_list')->where('lottery_id',$lottery_id)->count();//拿到抽奖活动参与人数
+        $Lotterynum = new Lotterynum();//计算开奖人数
+        $lotterymsg = new Lotterymsg();//发送开奖通知
+
+        if($state== 0 && $luck_mode ==0 && $partakecount >= $packed_lottery){
+              //满足条件，开始计算开奖
+              //计算中奖人数
+              $opennumber=$Lotterynum -> opennumber($lottery_id);
+              //先将全部人设置为未中奖和已经开奖
+              $dbreturn= db('lottery_partake_list')->where('lottery_id',$lottery_id)->update(['prize' => 1,'state' => 0]);
+
+              //再根据中奖人数，随机挑选设置为中奖
+              $sql="UPDATE lottery_partake_list SET prize=0 WHERE  lottery_id =".$lottery_id." and id in ( SELECT F.id FROM (SELECT * FROM lottery_partake_list ORDER BY RAND() LIMIT ".$opennumber.") F);";
+              // $Model = new Model();
+
+              $updatenum = Db::execute($sql); //执行，随机选取中奖人数
+              $updatestate= db('lottery_crowd_list')->where('id',$lottery_id)->update(['state' => 1]);//更新抽奖活动为已经，开奖
+
+              $lottery_data=db('lottery_crowd_list')->where('id',$lottery_id)->find(); //拿到抽奖活动信息
+              $goods_name=$lottery_data['goods_name'];//拿到奖品名称
+              //然后发送消息给参与的人
+              $text = $lotterymsg -> lotterymssage($lottery_id,$goods_name);
+
+        }
+        else{
+
+             return ['state'   => '200','message'  => "不满足条件" ];
+        }
+
+        return ['state'   => '200','message'  => "满人开奖成功" ];
+
+    }
+
+
+
+
+
+
+
 
 
 }
--- php/extend/processing/Lotterynum.php
+++ php/extend/processing/Lotterynum.php
@@ -1,27 +1,42 @@
 <?php
-namespace temmsg;
+namespace processing;
 use think\Db;
 use think\Request;
 use think\Log;
 
 class Lotterynum{
     /**
-     * 发送HTTP请求方法
-     * @param  string $data   抽奖的数据
+     * @param  string $lottery_id   抽奖的活动id
+     * *
      */
-    public function opennumber($lottery_data){
+    public function opennumber($lottery_id){
+
+        $partakecount=db('lottery_partake_list')->where('lottery_id',$lottery_id)->count();//拿到抽奖活动参与人数
+        $lottery_data=db('lottery_crowd_list')->where('id',$lottery_id)->find(); //拿到抽奖活动信息
+        $state=$lottery_data['state'];//拿到中奖概率值
+        if($partakecount < 1 ){
+            echo  json_encode(['state'   => '200','message'  => "无用户参与不能开奖" ] ) ;
+            die ();
+            // $state=['state'   => '400','message'  => "无用户参与不能开奖" ];
+            // return $state;
+        }
+        if($state == 1){
+            echo  json_encode(['state'   => '200','message'  => "已经开过奖了" ] ) ;
+            die ();
+        }
+        // return $lottery_data;
         $lottery_mode=$lottery_data['lottery_mode'];//拿到中奖方式
         if($lottery_mode ==0){
             //中奖方式为概率
             $lottery_probability=$lottery_data['lottery_probability'];//拿到中奖概率值
             $number=round($lottery_probability/100 * $partakecount);//计算出中奖人数值
-            if($number <= 0){
-                $number=1;
-            }
         }
         else{
             //中奖方式为人数
             $number=$lottery_data['lottery_number'];//中奖人数等于设置的中奖人数值
+        }
+        if($number <= 0){
+            $number=1;
         }
         return $number;
 
