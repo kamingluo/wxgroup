290a938524aeea9a1120a331fbeadb592f85c6d5
kaming <954087620@qq.com>
2020-04-13 00:16:26 +0800

    抽奖

--- wxminiapp/pages/group/lottery/pushlottery/pushlottery.js
+++ wxminiapp/pages/group/lottery/pushlottery/pushlottery.js
@@ -4,30 +4,115 @@ Page({
    * 页面的初始数据
    */
   data: {
-    //开奖方式选择
-    picker: ['满人开奖', '到时开奖'],
-    index: "0",
+    crowd_id:null,//群id
+    goodsname:'',//商品名称
     //上传图片
     uploaderList: [],
     uploaderNum: 0,
     showUpload: true,
+    score:10,//所需积分
+
+    //开奖方式选择
+    picker: ['满人开奖', '到时开奖'],
+    luck_mode: "0",
+    //开奖人数
+    packed_lottery:1,
     //开奖时间选择
     timepicker: [],
     time: 12,
     lotterytime: '12:00:00',
+    datetime: '2020-06-01',
+
+    //中奖方式
+    lottery_mode:"0",
+    openpicker: ['概率', '人数'],
+    lottery_probability:30,//中奖概率
+    lottery_number:10,//中奖人数
+
+    wxnumber:null,//群主微信
+    remarks:null,//备注
+    loadModal:false,//提示框
+
 
   },
   onLoad: function(options) {
-    this.timeicker()
+    this.setData({
+      crowd_id: options.crowd_id
+    })
 
+    this.timeicker()
+  },
+
+
+  goodsname:function(e){
+    this.setData({
+      goodsname: e.detail.value,
+    })
+
+  },
+  score:function(e){
+    this.setData({
+      score: e.detail.value,
+    })
   },
 
 
   PickerChange(e) {
-    console.log(e);
+    console.log(e.detail.value);
+    // 开奖方式，0是满人开奖，1是到时间开奖
     this.setData({
-      index: e.detail.value
+      luck_mode: e.detail.value
     })
+  },
+  DateChange:function(e){
+    this.setData({
+      datetime: e.detail.value
+    })
+
+  },
+
+  packedlottery:function(e){
+    this.setData({
+      packed_lottery: e.detail.value
+    })
+
+  },
+
+  lotteryPickerChange(e){
+    //中奖方式
+    this.setData({
+      lottery_mode: e.detail.value
+    })
+
+  },
+
+  lotteryprobability:function(e){
+    this.setData({
+      lottery_probability: e.detail.value
+    })
+
+  },
+
+
+  lotterynumber: function (e) {
+    this.setData({
+      lottery_number: e.detail.value
+    })
+
+  },
+
+  wxnumber:function(e){
+    this.setData({
+      wxnumber: e.detail.value
+    })
+
+  },
+
+  remarks: function (e) {
+    this.setData({
+      remarks: e.detail.value
+    })
+
   },
 
 
@@ -52,9 +137,6 @@ Page({
       lotterytime: this.data.timepicker[e.detail.value]
     })
   },
-
-
-
 
 
   //图片处理开始
@@ -110,7 +192,7 @@ Page({
   },
 
   //上传图片拿到链接
-  moredata: function() {
+  pushimg: function() {
     var that = this;
     var imgList = []; //多张图片地址，保存到一个数组当中
     var state = 0; //state记录当前已经上传到第几张图片
@@ -135,13 +217,107 @@ Page({
       }
     }).then(function(imgList) {
       console.log("多张图片返回结果上传数据库的", imgList[0])
-      let logo = imgList[0]
-      //that.creategroup(logo)
+      let imgurl = imgList[0];//拿到图片链接，然后触发上传
+      //that.createlottery(imgurl)
     })
+  },
+
+  createlottery: function (imgurl){
+    var that =this;
+    let crowd_id = that.data.crowd_id;//群id
+    let goodsname=that.data.goodsname;//奖品名称
+    // let imgurl = imgurl;//奖品图片
+    let score = that.data.score || 0;//奖励积分
+    let luck_mode =that.data.luck_mode;//开奖方式
+    let packed_lottery=that.data.packed_lottery;//开奖人数
+    let datetime = that.data.datetime;
+    let lotterytime = that.data.lotterytime;
+    let time_lottery = datetime + " " + lotterytime;//开奖时间
+
+    let lottery_mode=that.data.lottery_mode;//开奖方式
+
+    let lottery_probability=that.data.lottery_probability;//中奖概率
+    let lottery_number = that.data.lottery_number;//中奖人数
+    let wxnumber=that.data.wxnumber;//群主微信号
+    let remaks=that.data.remaks;//开奖备注
+
+
+
+
+
   },
 
 
 
+
+  //提交发布
+  pushlottery:function(){
+    let goodsname=this.data.goodsname;
+    if (!goodsname){
+      this.wxtoast("奖品名称不能为空")
+      return;
+    }
+    let uploaderNum =this.data.uploaderNum
+
+    if (uploaderNum == 0){
+      this.wxtoast("图片不能为空")
+      return;
+    }
+
+    let luck_mode = this.data.luck_mode;//开奖方式
+    if (luck_mode == 0){
+      let packed_lottery=this.data.packed_lottery;
+      if (packed_lottery < 1){
+        this.wxtoast("开奖人数错误")
+        return;
+      }
+    }
+    else{
+      let time1= new Date();//获取当前时间
+      let time2 = this.data.datetime;//设置的开奖时间
+     
+      let startTime = new Date(Date.parse(time1));
+      let endTime = new Date(Date.parse(time2));
+      if (startTime > endTime) {
+        this.wxtoast("开奖时间错误")
+        return;
+      }
+    }
+
+    let lottery_mode = this.data.lottery_mode;//中奖方式
+    if (lottery_mode ==0){
+      //概率
+      let lottery_probability = this.data.lottery_probability;//中奖概率
+      if (lottery_probability == null || lottery_probability < 1){
+        this.wxtoast("开奖概率值错误")
+        return;
+      }
+    }
+    else{
+      //人数
+      let lottery_number = this.data.lottery_number;//中奖概率
+      if (lottery_number == null || lottery_number < 1) {
+        this.wxtoast("开奖人数值错误")
+        return;
+      }
+    }
+
+  //都没有错误的情况下，执行上传图片
+    this.setData({
+      loadModal: true
+    })
+    this.pushimg()
+
+  },
+
+  wxtoast:function(title){
+    wx.showToast({
+      title: title,
+      icon: 'none',
+      duration: 2000,
+    })
+
+  },
 
 
 })
