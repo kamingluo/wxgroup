18d1900bdb1bd29976686ccd5adccbb982764f77
kaming <ljm@xlbzone.com>
2020-04-21 17:57:23 +0800

    小程序优化

--- wxminiapp/pages/group/pushnews/pushnews.js
+++ wxminiapp/pages/group/pushnews/pushnews.js
@@ -140,7 +140,8 @@ getPhoneNumber: function(e) {
     var that = this;
     wx.chooseImage({
       count: 9 - that.data.uploaderNum, // 默认9
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      // sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sizeType: ['compressed'], // 只有压缩图
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function(res) {
         console.log("返回选定照片的本地文件路径列表", res)
