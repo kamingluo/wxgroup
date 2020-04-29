c8edf6015b753e26917a6ffb8f39c511b724b7e4
kaming <ljm@xlbzone.com>
2020-04-29 14:28:28 +0800

    分页

--- php/application/miniapp/controller/group/Userdata.php
+++ php/application/miniapp/controller/group/Userdata.php
@@ -27,8 +27,19 @@ class Userdata
     {
         $crowd_id =$request->param("crowd_id");
         $user_id =$request->param("user_id");
-        $userscorelist =db('score_record')->where('user_id',$user_id)->where('crowd_id',$crowd_id)->order('id desc')->select();
-        $state=['state'   => '200','message'  => "查询用户对应群积分列表" ];
+        $pages=$request->param("pages");//页数
+        if($pages){
+            //有传pages
+            $newpages= ($pages-1) * 20;//一页20条
+            $userscorelist =db('score_record')->where('user_id',$user_id)->where('crowd_id',$crowd_id)->order('id desc')->limit($newpages,20)->select(); 
+        }
+        else{
+            //兼容没有传页数，返回全部信息
+            $userscorelist =db('score_record')->where('user_id',$user_id)->where('crowd_id',$crowd_id)->order('id desc')->select();
+        }
+        $count =db('score_record')->where('user_id',$user_id)->where('crowd_id',$crowd_id)->count();
+
+        $state=['state'   => '200','message'  => "查询用户对应群积分列表" ,'conut'=>$count ];
         $resdata=array_merge($state,array('userscorelist'=>$userscorelist));
         return $resdata ;
       
