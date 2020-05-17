7b8fb217ff6ab2603865256ca6e52e1c36d53e05
kaming <954087620@qq.com>
2020-05-17 23:44:31 +0800

    wss

--- php/start_for_win.bat
+++
@@ -1,4 +0,0 @@
-php Applications\push\start_register.php Applications\push\start_gateway.php Applications\push\start_businessworker.php
-pause
-
-php start_register.php start_gateway.php start_businessworker.php
