1e938bba0a60274efa5f58554490156ea47b8810
kaming <ljm@xlbzone.com>
2020-05-18 17:06:40 +0800

    wss

--- php/application/push/start_gateway.php
+++ php/application/push/start_gateway.php
@@ -22,7 +22,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 // gateway 进程，这里使用Text协议，可以用telnet测试
 // $gateway = new Gateway("tcp://0.0.0.0:8282");
-$gateway = new Gateway("websocket://47.106.253.110:8282");
+$gateway = new Gateway("websocket://0.0.0.0:8282");
 // gateway名称，status方便查看
 $gateway->name = 'pushGateway';
 // gateway进程数
@@ -50,21 +50,21 @@ $gateway->pingData = '{"type":"ping"}';
 
 
 // 当客户端连接上来时，设置连接的onWebSocketConnect，即在websocket握手时的回调
-$gateway->onConnect = function($connection)
-{
-    $connection->onWebSocketConnect = function($connection , $http_header)
-    {
-        // 可以在这里判断连接来源是否合法，不合法就关掉连接
-        // $_SERVER['HTTP_ORIGIN']标识来自哪个站点的页面发起的websocket链接
-        // if($_SERVER['HTTP_ORIGIN'] != 'http://kedou.workerman.net')
-        // {
-        //     $connection->close();
-        // }
-        // onWebSocketConnect 里面$_GET $_SERVER是可用的
-        // var_dump($_GET, $_SERVER);
-        $connection->send("kaming");
-    };
-}; 
+// $gateway->onConnect = function($connection)
+// {
+//     $connection->onWebSocketConnect = function($connection , $http_header)
+//     {
+//         // 可以在这里判断连接来源是否合法，不合法就关掉连接
+//         // $_SERVER['HTTP_ORIGIN']标识来自哪个站点的页面发起的websocket链接
+//         // if($_SERVER['HTTP_ORIGIN'] != 'http://kedou.workerman.net')
+//         // {
+//         //     $connection->close();
+//         // }
+//         // onWebSocketConnect 里面$_GET $_SERVER是可用的
+//         // var_dump($_GET, $_SERVER);
+//         $connection->send("kaming");
+//     };
+// }; 
 
 // 如果不是在根目录启动，则运行runAll方法
 if(!defined('GLOBAL_START'))
