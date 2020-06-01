5bbd8ac2488347cb8d863aca10fc5b25eb43695e
kaming <ljm@xlbzone.com>
2020-06-01 11:35:35 +0800

    七牛聊天空间

--- php/application/miniapp/controller/Currency.php
+++ php/application/miniapp/controller/Currency.php
@@ -49,15 +49,28 @@ class Currency
 
         $accessKey = Config('qiniuaccessKey');
         $secretKey = Config('qiniusecretKey');
-          $auth = new Auth($accessKey, $secretKey);
-          $bucket = 'material';
+        $auth = new Auth($accessKey, $secretKey);
+        $bucket = 'material';
           // 生成上传Token
-         $upToken  = $auth->uploadToken($bucket);
-
+        $upToken  = $auth->uploadToken($bucket);
         // return  $upToken;
          $ret = array('message' => "生成material七牛上传Token成功",'uptoken' => $upToken);
          return $ret;
      }
+
+    public function qiniugroupchatdata()
+     {
+ 
+         $accessKey = Config('qiniuaccessKey');
+         $secretKey = Config('qiniusecretKey');
+         $auth = new Auth($accessKey, $secretKey);
+         $bucket = 'groupchatdata';
+           // 生成上传Token
+         $upToken  = $auth->uploadToken($bucket);
+         // return  $upToken;
+          $ret = array('message' => "生成七牛groupchatdata空间上传Token成功",'uptoken' => $upToken);
+          return $ret;
+      }
 
 
   public function formid(Request $request)
