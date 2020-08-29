9fc90f94b5b9c613407abf9d90e766aa4b41057b
kaming <954087620@qq.com>
2020-08-29 15:35:30 +0800

    商城首页

--- php/application/miniapp/controller/pdd/Recommend.php
+++ php/application/miniapp/controller/pdd/Recommend.php
@@ -29,9 +29,11 @@ class Recommend
 
   //运营频道商品查询API：pdd.ddk.goods.recommend.get
   //参数channel_type:0-1.9包邮, 1-今日爆款, 2-品牌好货,3-相似商品推荐,4-猜你喜欢,5-实时热销,6-实时收益,7-今日畅销,8-高佣榜单，默认1
+  //参数cat_id,猜你喜欢场景的商品类目，20100-百货，20200-母婴，20300-食品，20400-女装，20500-电器，20600-鞋包，20700-内衣，20800-美妆，20900-男装，21000-水果，21100-家纺，21200-文具,21300-运动,21400-虚拟,21500-汽车,21600-家装,21700-家具,21800-医药
 public function recommend(Request $request){
-      $pages=$request->param("pages");//商品id
-      $channel_type=$request->param("channel_type");//商品id
+      $pages=$request->param("pages");//页数
+      $channel_type=$request->param("channel_type");//类型1
+      $cat_id=$request->param("channel_type");//类型2
       if(!$pages){
         $pages=0;
       }
@@ -43,6 +45,7 @@ public function recommend(Request $request){
       $p_id=Config('pdd_p_id');
       $data=array(
           "limit"=>20,
+          "cat_id"=>$cat_id,
           "channel_type"=>$channel_type,
           "p_id"=>$p_id,
           "offset"=>$offset,
--- wxminiapp/pages/extension/extension.js
+++ wxminiapp/pages/extension/extension.js
@@ -5,6 +5,64 @@ Page({
    * 页面的初始数据
    */
   data: {
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+    }, {
+      id: 1,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+    }, {
+      id: 4,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+    }, {
+      id: 5,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+    }, {
+      id: 6,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+    }],
+
+    iconList: [{
+      icon: 'cardboardfill',
+      color: 'red',
+      badge: 120,
+      name: '新人免单'
+    }, {
+      icon: 'recordfill',
+      color: 'orange',
+      badge: 1,
+      name: '9.9包邮'
+    }, {
+      icon: 'picfill',
+      color: 'yellow',
+      badge: 0,
+      name: '大牌清仓'
+    }, {
+      icon: 'noticefill',
+      color: 'olive',
+      badge: 22,
+      name: '高佣精选'
+    }, {
+      icon: 'upstagefill',
+      color: 'cyan',
+      badge: 0,
+      name: '百亿补贴'
+    }],
+    TabCur: 0,
+    scrollLeft: 0
 
   },
 
@@ -16,28 +74,29 @@ Page({
     let mall_type=options.mall_type;
     let goods_id=options.goods_id;
     let search_id=options.search_id;
+
+    //分享进来，直接跳转到商品详情页面
     if(mall_type==1){
       wx.navigateTo({
         url: '/pages/extension/pdd/goodsdetails/goodsdetails?goods_id=' + goods_id + '&search_id=' + search_id
       })
     }
-
-
   },
 
-  
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
+  //点击跳转到搜索页面
   gosearch:function(){
     wx.navigateTo({
       url: '/pages/extension/pdd/search/search'
     })
+  },
 
+//切换导航栏
+  tabSelect(e) {
+    console.log("切换导航栏",e)
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+    })
   },
 
   /**
@@ -47,10 +106,4 @@ Page({
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
