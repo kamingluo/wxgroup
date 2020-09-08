da6021f73048ccaf163273995935f66be9ef19a4
kaming <ljm@xlbzone.com>
2020-09-08 14:06:49 +0800

    我的页面改版

--- php/application/miniapp/controller/appdata/My.php
+++ php/application/miniapp/controller/appdata/My.php
@@ -9,8 +9,8 @@ class My
     public function xiaouad(Request $request)
     {
         $data=array(
-            ['id'=>'1','title'=>'这是标题1','text'=>'这是内容1','bgcolor'=>'bg-gradual-green','icon'=>'http://qiniu.luojiaming.vip/xiaou.jpg','jumpurl'=>'/pages/webview/webview?url=https://mp.weixin.qq.com/s/IUmUFZmwdyFnPYTMX4n7cw'],
-            ['id'=>'2','title'=>'这是标题2','text'=>'这是内容2','bgcolor'=>'bg-gradual-blue','icon'=>'http://qiniu.luojiaming.vip/tuiguang03.jpg','jumpurl'=>'/pages/business/gdtad/gdtad']);
+            ['id'=>'1','title'=>'火把小程序','imageurl'=>'http://qiniu.luojiaming.vip/myhuoba.png','jumpurl'=>'/pages/webview/webview?url=https://mp.weixin.qq.com/s/IUmUFZmwdyFnPYTMX4n7cw'],
+            ['id'=>'2','title'=>'一起学堂','imageurl'=>'http://qiniu.luojiaming.vip/myxuetang.png','jumpurl'=>'/pages/business/gdtad/gdtad']);
         $state=['state'   => '200','message'  => '我的页面小U广告' ];
         $resdata=array_merge($state,array('data'=>$data));
     	return  $resdata ;
--- wxminiapp/pages/my/my.js
+++ wxminiapp/pages/my/my.js
@@ -12,6 +12,7 @@ const baseConfig = require('./../../utils/config.js')//配置文件
 
 Page({
   data: {
+    imageurl:'https://group.gzywudao.top/php/public/',//默认图片链接
   },
   onLoad: function (e) {
     let imageurl = baseConfig.imageurl;
