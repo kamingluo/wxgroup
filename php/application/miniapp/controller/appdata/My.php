559345d86fccb8d03be35d7df093343ce7686e36
kaming <ljm@xlbzone.com>
2020-09-09 11:31:42 +0800

    我的页面改版

--- php/application/miniapp/controller/appdata/My.php
+++ php/application/miniapp/controller/appdata/My.php
@@ -9,8 +9,8 @@ class My
     public function xiaouad(Request $request)
     {
         $data=array(
-            ['id'=>'1','title'=>'火把小程序','type'=>1,'imageurl'=>'http://qiniu.luojiaming.vip/myhuoba.png','url'=>'/pages/webview/webview?url=https://mp.weixin.qq.com/s/IUmUFZmwdyFnPYTMX4n7cw'],
-            ['id'=>'2','title'=>'一起学堂','type'=>1,'imageurl'=>'http://qiniu.luojiaming.vip/myxuetang.png','url'=>'/pages/business/gdtad/gdtad']);
+            ['id'=>'1','title'=>'火把小程序','type'=>1,'imageurl'=>'http://qiniu.luojiaming.vip/myhuoba.png','url'=>'/pages/webview/webview?url=https://mp.weixin.qq.com/s/pGMr4pEV-d-tm0ZcT0gO-Q'],
+            ['id'=>'2','title'=>'一起学堂','type'=>1,'imageurl'=>'http://qiniu.luojiaming.vip/myxuetang.png','url'=>'/pages/webview/webview?url=https://mp.weixin.qq.com/s/vyyu_5qibCYs4ym8ZV9uWw']);
         $state=['state'   => '200','message'  => '我的页面小U广告' ];
         $resdata=array_merge($state,array('data'=>$data));
     	return  $resdata ;
@@ -23,7 +23,7 @@ class My
         $data=array(
             ['id'=>'1','title'=>'去赚金币','type'=>1,'imageurl'=>'miniapp/images/my/moreicon/zhuanjinbi.png','url'=>'/pages/business/gdtad/gdtad'],
             ['id'=>'2','title'=>'金币兑换','type'=>1,'imageurl'=>'miniapp/images/my/moreicon/duihuan.png','url'=>'/pages/business/exchange/exchange'],
-            ['id'=>'3','title'=>'加入我们','type'=>1,'imageurl'=>'miniapp/images/my/moreicon/jiaruwomen.png','url'=>'/pages/joinweChat/joinweChat'],
+            ['id'=>'3','title'=>'加入我们','type'=>1,'imageurl'=>'miniapp/images/my/moreicon/jiaruwomen.png','url'=>'/pages/webview/webview?url=https://qing.utaojs.com/app/index.php?i=3&c=entry&id=1&do=index&m=julan_qrg&wxref=mp.weixin.qq.com#wechat_redirect'],
             ['id'=>'4','title'=>'在线客服','type'=>'kefu','imageurl'=>'miniapp/images/my/moreicon/kefu.png','url'=>'/pages/business/gdtad/gdtad'],
             ['id'=>'5','title'=>'意见建议','type'=>'jianyi','imageurl'=>'miniapp/images/my/moreicon/fankui.png','url'=>'/pages/business/gdtad/gdtad'],
         );
