6c24e6821a368092e57b672109138f589ed7a10f
kaming <ljm@xlbzone.com>
2020-09-17 17:22:55 +0800

    修炼测试

--- php/application/miniapp/controller/Test.php
+++ php/application/miniapp/controller/Test.php
@@ -12,13 +12,23 @@ class Test
 
         return "ceshi";
        
+        // $lottery = new Lotterymsg();
+        // $lottery_id=1;
+        // $goods_name="这是商品名称啊";
+        // $text = $lottery -> lotterymssage($lottery_id,$goods_name);
+        // return $text;
+
+    }
+
+    public function xiulian(Request $request)
+    {
+
+        $data=$request->param();//用户id
 
 
 
-
-
-
-
+        return $data;
+       
         // $lottery = new Lotterymsg();
         // $lottery_id=1;
         // $goods_name="这是商品名称啊";
