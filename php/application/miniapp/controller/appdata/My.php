44ed292f1b62900b19fec17250947329e1d307c3
kaming <954087620@qq.com>
2020-10-17 19:00:11 +0800

    屏蔽小U广告

--- php/application/miniapp/controller/appdata/My.php
+++ php/application/miniapp/controller/appdata/My.php
@@ -9,8 +9,9 @@ class My
     public function xiaouad(Request $request)
     {
         $data=array(
-            ['id'=>'1','title'=>'火把小程序','type'=>1,'imageurl'=>'http://qiniu.luojiaming.vip/myhuoba.png','url'=>'/pages/webview/webview?url=https://mp.weixin.qq.com/s/pGMr4pEV-d-tm0ZcT0gO-Q'],
-            ['id'=>'2','title'=>'一起学堂','type'=>1,'imageurl'=>'http://qiniu.luojiaming.vip/myxuetang.png','url'=>'/pages/webview/webview?url=https://mp.weixin.qq.com/s/vyyu_5qibCYs4ym8ZV9uWw']);
+            // ['id'=>'1','title'=>'火把小程序','type'=>1,'imageurl'=>'http://qiniu.luojiaming.vip/myhuoba.png','url'=>'/pages/webview/webview?url=https://mp.weixin.qq.com/s/pGMr4pEV-d-tm0ZcT0gO-Q'],
+            // ['id'=>'2','title'=>'一起学堂','type'=>1,'imageurl'=>'http://qiniu.luojiaming.vip/myxuetang.png','url'=>'/pages/webview/webview?url=https://mp.weixin.qq.com/s/vyyu_5qibCYs4ym8ZV9uWw']
+          );
         $state=['state'   => '200','message'  => '我的页面小U广告','xiaouadtitle'  => '社群工具推荐' ];
         $resdata=array_merge($state,array('data'=>$data));
     	return  $resdata ;
