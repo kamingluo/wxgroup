bab706a0d5b289f17523b7694b9a5a26ee7c31c1
kamingluo <954087620@qq.com>
2021-04-16 10:44:28 +0800

    限时任务审核后台

--- php/application/saas/controller/configure/Limittasks.php
+++ php/application/saas/controller/configure/Limittasks.php
@@ -31,6 +31,7 @@ class Limittasks
       $token=$request->param("token");
       $pages=$request->param("pages");
       $state=$request->param("state");
+      $limit_id=$request->param("limit_id");
       $id=havecrowdid($token);
       if($pages == 1 || $pages==null  ){
         $number=0;
@@ -38,16 +39,64 @@ class Limittasks
       else{
         $number=($pages - 1)*10 ;
       }
-
-      $data=db('corwd_limit_task_record')->where('crowd_id',$id)->where('state',$state)->order('id ASC')->limit($number,10)->select();
-      $countnumber=db('task_record')->where('crowd_id',$id)->where('state',$state)->count();
+      $data=db('corwd_limit_task_record')->where('crowd_id',$id)->where('state',$state)->where('limit_id',$limit_id)->order('id ASC')->limit($number,10)->select();
+      $countnumber=db('task_record')->where('crowd_id',$id)->where('state',$state)->where('limit_id',$limit_id)->count();
       $state=['state'   => '200','message'  => "限时任务群员提交任务列表" ];
       $resdata=array_merge($state,array('countnumber'=>$countnumber),array('data'=>$data));
       return $resdata ;
+    }
+    
 
+    //审核任务
+    public function audittask(Request $request){
+      $token=$request->param("token");
+      $result=$request->param("result");
+      $state=$request->param("state");
+      $limit_id=$request->param("limit_id");
+      $time =date('Y-m-d H:i:s',time());//获取当前时间
+      $crowd_id=havecrowdid($token);
+      $data=db('corwd_limit_task_record')->where('limit_id',$limit_id)->where('crowd_id',$crowd_id)->where('state',0)->find();
+      if($data==null){
+        $state=['state'   => '400','message'  => "未找到任务或者已经审核" ];
+        return $state;
+      }
+      if($state == 1){
+        $score=$data["score"];
+        $user_id=$data["user_id"];
+        //修改任务状态
+        $dbreturn= db('corwd_limit_task_record')->where('id',$limit_id)->update(['state' => 1,'result' => $result]);
 
+        //给用户相应的群积分账户加积分
+        $addscore= db('user_crowd')->where('user_id',$user_id)->where('crowd_id',$crowd_id)->setInc('score',$score);
+
+        //给用户增加积分记录
+        $user_data=db('user')->where('id',$user_id)->find(); //拿到用户信息
+        $score_record_data = ['id'=>'','openid' =>$user_data["openid"],'user_id' =>$user_id,'crowd_id' =>$crowd_id,'score' =>$score,'explain' => "群限时任务奖励",'state' => 0,'create_time' =>$time];
+        $score_record_id=db('score_record')->insert($score_record_data);
+
+        $state=['state'   => '200','message'  => "审核完成，结果为通过" ];
+        $resdata=array_merge($state,array('taskstate'=>'success'));
+        return $resdata ;
+        
+
+      }
+      else{
+        //审核不通过
+        $dbreturn= db('corwd_limit_task_record')->where('id',$limit_id)->update(['state' => 2,'result' => $result]);
+        $state=['state'   => '200','message'  => "审核完成，结果为不通过" ];
+        $resdata=array_merge($state,array('taskstate'=>'err'));
+        return $resdata;
+      }
 
     }
+    
+
+
+
+
+
+
+
 
 
 
