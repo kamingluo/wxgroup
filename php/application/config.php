bf2535a0105485a3de9319a622c4b2cfa67c71f4
kamingluo <954087620@qq.com>
2021-06-15 10:48:14 +0800

    防止sql注入

--- php/application/config.php
+++ php/application/config.php
@@ -41,7 +41,13 @@ return [
     // 是否开启多语言
     'lang_switch_on'         => false,
     // 默认全局过滤方法 用逗号分隔多个
-    'default_filter'         => '',
+    'default_filter'         => 'htmlspecialchars,addslashes',
+    // 默认全局过滤方法 用逗号分隔多个
+    //'default_filter' => 'htmlspecialchars,addslashes,strip_tags',
+    // htmlspecialchars：防XSS攻击，尖括号等转义过滤
+    // addslashes：防SQL注入，在每个双引号（"）前添加反斜杠
+    // strip_tags：剥去字符串中的 HTML 标签
+
     // 默认语言
     'default_lang'           => 'zh-cn',
     // 应用类库后缀
