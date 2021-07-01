b9db1352c7154736433464fe24fdc799f45418bf
kamingluo <954087620@qq.com>
2021-07-01 11:27:30 +0800

    paiming

--- php/application/miniapp/controller/group/Score.php
+++ php/application/miniapp/controller/group/Score.php
@@ -17,7 +17,7 @@ class Score
       }
       $endnumber=$pages*20 ; //结束查询条数
       $startnumber=$endnumber -20;//开始查询条数
-      $sql = "select user.nickName,user.avatarUrl,user_crowd.score from user,user_crowd where user.id=user_crowd.user_id and user_crowd.crowd_id = ".$crowd_id." order BY user_crowd.score desc,user_crowd.id asc LIMIT ".$startnumber.",20;";
+      $sql = "select user.nickName,user.avatarUrl,user_crowd.score from user,user_crowd where user.id=user_crowd.user_id and user_crowd.crowd_id = ".$crowd_id." order BY user_crowd.score desc,user_crowd.id desc LIMIT ".$startnumber.",20;";
       $groupuserlist = Db::query($sql); //拿到数据
       $count =db('user_crowd')->where('crowd_id',$crowd_id)->count();
       $state=['state'   => '200','message'  => "查询群用户积分排名成功",'count'=>$count ];
@@ -34,7 +34,7 @@ class Score
       //查询用户的群积分数
       $score=db('user_crowd')->where('user_id',$user_id)->where('crowd_id',$crowd_id)->value("score");
 
-      $sql="select count(1) as userrank from user_crowd  where  crowd_id = ".$crowd_id." and score >= (select score from user_crowd where user_id = ".$user_id." and crowd_id=".$crowd_id." order BY id asc )";
+      $sql="select count(1) as userrank from user_crowd  where  crowd_id = ".$crowd_id." and score >= (select score from user_crowd where user_id = ".$user_id." and crowd_id=".$crowd_id." order BY id desc )";
       $userrank = Db::query($sql); //拿到数据
       $newrank=$userrank[0]["userrank"];
       $state=['state'   => '200','message'  => "查询用户的积分所在名次跟积分数",'score'=>$score,'rank'=>$newrank ];
