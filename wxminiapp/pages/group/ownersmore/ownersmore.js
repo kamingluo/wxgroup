20ead298b96e013ab2ba4c650db8d3a8b9431299
kamingluo <954087620@qq.com>
2021-10-14 19:38:20 +0800

    更换图片链接

--- wxminiapp/pages/group/ownersmore/ownersmore.js
+++ wxminiapp/pages/group/ownersmore/ownersmore.js
@@ -7,25 +7,25 @@ Page({
     crowd_name: null,
     list: [{
         title: '群用户管理',
-        img: 'https://image.weilanwl.com/color2.0/plugin/sylb2244.jpg',
+        img: 'http://groupmaterial.gzywudao.top/moreuser.png',
         introduce: "管理群用户，查询信息，积分操作",
         url: '/pages/group/user/user?crowd_id='
       },
       {
         title: '群员全部任务',
-        img: 'https://image.weilanwl.com/color2.0/plugin/wdh2236.jpg',
+        img: 'http://groupmaterial.gzywudao.top/morealltask.png',
         introduce: "查询所有用户提交的任务记录",
         url: '/pages/group/alltasklists/alltasklists?crowd_id='
       },
       {
         title: '群签到设置',
-        img: 'https://image.weilanwl.com/color2.0/plugin/qpct2148.jpg',
+        img: 'http://groupmaterial.gzywudao.top/moreste.png',
         introduce: "设置群签到配置信息",
         url: '/pages/group/signseting/signseting?crowd_id='
       },
       {
         title: '群抽奖处理',
-        img: 'https://image.weilanwl.com/color2.0/plugin/qpczdh2307.jpg',
+        img: 'http://groupmaterial.gzywudao.top/moreluckdraw.png',
         introduce: "处理抽奖结果，发奖记录",
         url: '/pages/group/lottery/lotterylist/lotterylist?crowd_id='
       },
@@ -37,7 +37,7 @@ Page({
       },
       {
         title: '任务提交排行榜',
-        img: 'https://image.weilanwl.com/color2.0/plugin/cjkz2329.jpg',
+        img: 'http://groupmaterial.gzywudao.top/moreranking.png',
         introduce: "查看群员的任务提交情况",
         url: '/pages/group/tasksranking/tasksranking?crowd_id='
       },
