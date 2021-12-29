434bb117d78131b8cd239da86b79059e2fe85b44
kaming <954087620@qq.com>
2021-12-29 23:08:28 +0800

    任务标题

--- php/application/miniapp/controller/group/Handlelimittask.php
+++ php/application/miniapp/controller/group/Handlelimittask.php
@@ -18,14 +18,14 @@ class Handlelimittask
           //有传pages
           $newpages= ($pages-1) * 20;//一页20条
           //$dballtasklists =db('corwd_limit_task_record')->where('crowd_id',$crowd_id)->where('nickName','like',$newkeyword)->order('id desc')->limit($newpages,20)->select();
-          $dballtasklists = db()->table(array('corwd_limit_task_record'=>'t1','user'=>'t2'))->field('t1.*,t2.nickName,t2.avatarUrl')->where('t1.user_id=t2.id')->where('t1.crowd_id',$crowd_id)->where('t2.nickName','like',$newkeyword)->order('id desc')->limit($newpages,20)->select();
+          $dballtasklists = db()->table(array('corwd_limit_task_record'=>'t1','user'=>'t2','crowd_limit_tasks'=>'t3'))->field('t1.*,t2.nickName,t2.avatarUrl,t3.title')->where('t1.user_id=t2.id')->where('t1.crowd_id',$crowd_id)->where('t1.limit_id=t3.id')->where('t2.nickName','like',$newkeyword)->order('id desc')->limit($newpages,20)->select();
       }
       else{
           //兼容没有传页数，返回全部信息
-          $dballtasklists = db()->table(array('corwd_limit_task_record'=>'t1','user'=>'t2'))->field('t1.*,t2.nickName,t2.avatarUrl')->where('t1.user_id=t2.id')->where('t1.crowd_id',$crowd_id)->where('t2.nickName','like',$newkeyword)->order('id desc')->select();
+          $dballtasklists = db()->table(array('corwd_limit_task_record'=>'t1','user'=>'t2','crowd_limit_tasks'=>'t3'))->field('t1.*,t2.nickName,t2.avatarUrl,t3.title')->where('t1.user_id=t2.id')->where('t1.crowd_id',$crowd_id)->where('t1.limit_id=t3.id')->where('t2.nickName','like',$newkeyword)->order('id desc')->select();
       }
 
-      $count =db()->table(array('corwd_limit_task_record'=>'t1','user'=>'t2'))->field('t1.*,t2.nickName,t2.avatarUrl')->where('t1.user_id=t2.id')->where('t1.crowd_id',$crowd_id)->where('t2.nickName','like',$newkeyword)->count();
+      $count =db()->table(array('corwd_limit_task_record'=>'t1','user'=>'t2','crowd_limit_tasks'=>'t3'))->field('t1.*,t2.nickName,t2.avatarUrl,t3.title')->where('t1.user_id=t2.id')->where('t1.crowd_id',$crowd_id)->where('t1.limit_id=t3.id')->where('t2.nickName','like',$newkeyword)->count();
 
       $state=['state'   => '200','message'  => "查询该群的全部限时任务",'count'=>$count ];
       $resdata=array_merge($state,array('alltasklists'=>$dballtasklists));
