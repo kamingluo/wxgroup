b2b53ef544a3f47f2f9d35f81640210974a9f203
kamingluo <954087620@qq.com>
2022-07-28 16:23:10 +0800

    下载用户积分排行

--- php/application/saas/controller/configure/Scorelist.php
+++ php/application/saas/controller/configure/Scorelist.php
@@ -29,4 +29,76 @@ class Scorelist
     }
 
 
+
+  //下载粉丝的积分增长数据
+
+  public function socerranking(Request $request)
+  {
+    $token=$request->param("token");
+    $start_time=$request->param("start_time");
+    $end_time=$request->param("end_time");
+    $crowd_id=havecrowdid($token);//群id
+    $sendmode=0;//发送方式，0是直接下载列表。1是发送到邮箱
+
+
+    //from score_record WHERE crowd_id=3514 and state=0 and create_time >" + $start_time + " create_time < "+  $end_time + " GROUP BY user_id) as e
+
+    $oldlist=$sql = "SELECT `user`.id,`user`.nickName,e.score 
+     FROM `user`,(select user_id,SUM(score) as score
+     from score_record WHERE crowd_id= "+ $crowd_id +" and state=0 and create_time between " + $start_time + " and "+ $end_time + " GROUP BY user_id) as e
+     WHERE e.user_id=`user`.id ORDER BY e.score  DESC ;";
+     $list = Db::query($sql); //拿到数据
+
+	 $file_name = date('Y-m-d_His').'.xls';
+     $path = dirname(__FILE__); //找到当前脚本所在路径
+     Loader::import('PHPExcel.php'); //加载所需的类文件，必须引入 use think\Loader;命名空间，否则loader无法加载
+     Loader::import('PHPExcel.Reader.Excel2007'); 
+
+     $title="用户积分获取排名,开始计算时间:" + $start_time + "----结束计算时间" + $end_time;
+
+     $PHPExcel = new \PHPExcel();
+        $PHPSheet = $PHPExcel->getActiveSheet();
+        $PHPSheet->setTitle($title);
+        $PHPSheet->setCellValue("A1","排名");
+        $PHPSheet->setCellValue("B1","用户id");
+        $PHPSheet->setCellValue("C1","微信昵称");
+        $PHPSheet->setCellValue("D1","获得积分总数");
+        $i = 2;
+        $num = 1;
+		foreach($list as $key => $value){
+          $PHPSheet->setCellValue('A'.$i,''.$num);
+        	$PHPSheet->setCellValue('B'.$i,''.$value['id']);
+        	$PHPSheet->setCellValue('C'.$i,''.$value['nickName']);
+        	$PHPSheet->setCellValue('D'.$i,''.$value['score']);
+        	$i++;
+          $num++;
+    	}
+        $PHPWriter = \PHPExcel_IOFactory::createWriter($PHPExcel,"Excel2007");
+        header('Content-Disposition: attachment;filename='.$file_name);
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+        if($sendmode == 0){
+            $PHPWriter->save("php://output");  
+            return "直接输出报告";
+
+        }
+        else{
+            $user_email=$request->param("user_email");//接受邮箱
+            $excelpath='./excel/'.$file_name;
+            $PHPWriter->save($excelpath);//保存到服务器指定路径
+            $emaildata=sendEmail([['user_email'=>$user_email,'content'=>'群记分群员积分获取排名','excel'=>$excelpath]]);
+            $state=['state'   => '200','message'  => "邮件发送成功，请注意查收",'emaildata'  => $emaildata];
+             return  $state;    
+        }
+        
+        
+    }
+
+
+
+
+
+
+
+
 }
--- saas/src/components/page/Scorelist.vue
+++ saas/src/components/page/Scorelist.vue
@@ -6,32 +6,27 @@
     <div class="crumbs">
       <el-breadcrumb separator="/">
         <el-breadcrumb-item style="font-size:18px;">
-          <i class="el-icon-lx-cascades"></i> 用户积分流水</el-breadcrumb-item>
+          <i class="el-icon-lx-cascades"></i> 用户积分流水
+        </el-breadcrumb-item>
       </el-breadcrumb>
-      
+
     </div>
     <div class="container">
 
-    <div class="handle-box">
+      <div class="handle-box">
 
-       <el-input
-          v-model="nickName"
-          placeholder="用户昵称(支持模糊查询)"
-          class="handle-input"
-        ></el-input>
-        <el-button type="primary" icon="search" class="search jianju" @click="search"
-          >搜索</el-button
-        >
-        <el-button type="primary" icon="search" class="search" @click="newsearch"
-          >重置</el-button
-        >
+        <el-input v-model="nickName" placeholder="用户昵称(支持模糊查询)" class="handle-input"></el-input>
+        <el-button type="primary" icon="search" class="search jianju" @click="search">搜索</el-button>
+        <el-button type="primary" icon="search" class="search" @click="newsearch">重置</el-button>
+
+        <el-button type="primary" icon="search" class="search" @click="socerranking">统计下载用户积分获取情况</el-button>
 
       </div>
 
       <div class="handle-box"></div>
       <el-table :data="tableData" border class="table" ref="multipleTable">
-         <el-table-column prop="user_id" label="用户id" width="100">
-         </el-table-column>
+        <el-table-column prop="user_id" label="用户id" width="100">
+        </el-table-column>
         <el-table-column prop="nickName" label="用户昵称" width="120">
         </el-table-column>
         <el-table-column prop="avatarUrl" label="用户头像" width="120">
@@ -46,21 +41,16 @@
         </el-table-column>
 
         <el-table-column prop="state" label="积分类型" width="100">p
-        <template slot-scope="scope">
-        <p>{{scope.row.state==0?"增加积分":scope.row.state==1?"减少积分":"其他"}}</p>
-         </template>
+          <template slot-scope="scope">
+            <p>{{scope.row.state==0?"增加积分":scope.row.state==1?"减少积分":"其他"}}</p>
+          </template>
         </el-table-column>
         <el-table-column prop="create_time" label="操作时间" width="160">
         </el-table-column>
       </el-table>
-       <!-- 换页 -->
+      <!-- 换页 -->
       <div class="pagination">
-        <el-pagination
-          background
-          @current-change="handleCurrentChange"
-          layout="prev, pager, next"
-          :total="datapages"
-        >
+        <el-pagination background @current-change="handleCurrentChange" layout="prev, pager, next" :total="datapages">
         </el-pagination>
       </div>
     </div>
@@ -72,177 +62,242 @@
     </el-dialog>
 
 
-    
 
 
- 
+    <!-- 下载弹出框 -->
+    <el-dialog title="统计下载用户积分获取情况" :visible.sync="socerrankingModel" width="30%">
+      <el-form ref="form"  label-width="80px">
+        <el-date-picker v-model="timeslot"  type="datetimerange" value-format="yyyy-MM-dd HH:mm:ss" range-separator="至" start-placeholder="开始日期"
+          end-placeholder="结束日期">
+        </el-date-picker>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="socerrankingModel = false">取 消</el-button>
+        <el-button type="primary" @click="Downloadfile">确 定</el-button>
+      </span>
+    </el-dialog>
+
+
+
+
+
 
   </div>
 </template>
 
 <script>
-export default {
-  name: "basetable",
-  data() {
-    return {
-      url: "./static/vuetable.json",
-      tableData: [],
-      cur_page: 1,
-      multipleSelection: [],
-      del_list: [],
-      editVisible: false,
-      form: {},
-      idx: -1,
-      delVisible: false,
-      imgVisible: false,
-      yulanimg: null,
-      datapages: 0,
-      nickName: "", //查询昵称
-    };
-  },
-  created() {
-    this.getData();
-  },
-
-  computed: {
+  export default {
+    name: "basetable",
     data() {
-      return this.tableData.filter((d) => {
-        let is_del = false;
-        for (let i = 0; i < this.del_list.length; i++) {
-          if (d.name === this.del_list[i].name) {
-            is_del = true;
-            break;
-          }
-        }
-      });
-    },
-  },
-  methods: {
-    // 分页导航
-    handleCurrentChange(val) {
-      this.cur_page = val;
-      this.getData(val);
-    },
-
-    //获取数据
-    async getData() {
-      console.log("获取数据")
-      let url = "configure/scorelist/userscorerecord";
-      let token = localStorage.getItem("token");
-      let params = {
-        pages: this.cur_page,
-        nickName: this.nickName,
-        token: token,
+      return {
+        url: "./static/vuetable.json",
+        tableData: [],
+        cur_page: 1,
+        multipleSelection: [],
+        del_list: [],
+        editVisible: false,
+        form: {},
+        idx: -1,
+        delVisible: false,
+        imgVisible: false,
+        yulanimg: null,
+        datapages: 0,
+        nickName: "", //查询昵称
+        timeslot: [],
+        socerrankingModel: false,
       };
-      try {
-        const res = await this.$axios.post(url, params);
-        console.log(res.data)
-        if (res.status == "200") {
-          this.tableData = res.data.data;
-          this.datapages = res.data.countnumber;
-        } else {
-          this.$message({
-            message: res.message,
-            type: "error",
-          });
-          console.log(res.message);
+    },
+    created() {
+      this.getData();
+    },
+
+    computed: {
+      data() {
+        return this.tableData.filter((d) => {
+          let is_del = false;
+          for (let i = 0; i < this.del_list.length; i++) {
+            if (d.name === this.del_list[i].name) {
+              is_del = true;
+              break;
+            }
+          }
+        });
+      },
+    },
+    methods: {
+      // 分页导航
+      handleCurrentChange(val) {
+        this.cur_page = val;
+        this.getData(val);
+      },
+
+
+      //获取数据
+      async getData() {
+        console.log("获取数据")
+        let url = "configure/scorelist/userscorerecord";
+        let token = localStorage.getItem("token");
+        let params = {
+          pages: this.cur_page,
+          nickName: this.nickName,
+          token: token,
+        };
+        try {
+          const res = await this.$axios.post(url, params);
+          console.log(res.data)
+          if (res.status == "200") {
+            this.tableData = res.data.data;
+            this.datapages = res.data.countnumber;
+          } else {
+            this.$message({
+              message: res.message,
+              type: "error",
+            });
+            console.log(res.message);
+          }
+        } catch (error) {
+          console.log(error);
         }
-      } catch (error) {
-        console.log(error);
-      }
-    },
+      },
 
-    search() {
-      this.getData();
-    },
+      //用户积分情况
+      socerranking() {
+        this.socerrankingModel = true;
+      },
 
-    newsearch() {
-      this.nickName = "";
-      this.cur_page = 1;
-      this.datapages = 0;
-      this.getData();
-    },
 
-    //放大图片
-    clickimg(e) {
-      console.log("点击图片", e.target.src);
-      this.imgVisible = true;
-      this.yulanimg = e.target.src;
-    },
+      //下载数据
+      async Downloadfile() {
+        let time=this.timeslot;
+        console.log("下载数据")
+        console.log(time)
+        if(!time){
+          this.$message.error("请选择时间");
+          return;
+        }
 
-  },
-};
+        let start_time=time[0];
+        let end_time=time[1];
+
+        let url = "configure/scorelist/socerranking";
+        let token = localStorage.getItem("token");
+        let params = {
+          start_time: start_time,
+          end_time: end_time,
+          token: token,
+        };
+        try {
+          const res = await this.$axios.post(url, params);
+          this.$message.success(`下载完成`);
+          
+        } catch (error) {
+          console.log(error);
+          this.$message.error("下载失败，请稍后重试或者联系管理人员");
+        }
+        
+      },
+
+
+      search() {
+        this.getData();
+      },
+
+      newsearch() {
+        this.nickName = "";
+        this.cur_page = 1;
+        this.datapages = 0;
+        this.getData();
+      },
+
+      //放大图片
+      clickimg(e) {
+        console.log("点击图片", e.target.src);
+        this.imgVisible = true;
+        this.yulanimg = e.target.src;
+      },
+
+    },
+  };
 </script>
 
 <style scoped>
-.yulan {
-  width: 440px;
-  height: 600px;
-}
-/* .imgbody image{
+  .yulan {
+    width: 440px;
+    height: 600px;
+  }
+
+  /* .imgbody image{
 width: 100px;
 height: 130px;
 } */
 
-.jianju {
-  margin-left: 10px;
-}
+  .jianju {
+    margin-left: 10px;
+  }
 
-.changdu {
-  width: 300px;
-}
+  .changdu {
+    width: 300px;
+  }
 
-.taskimg {
-  width: 140px;
-  height: 180px;
-  margin-left: 15px;
-  margin-bottom: 10rpx;
-}
-.logo {
-  width: 70px;
-  height: 70px;
-}
+  .taskimg {
+    width: 140px;
+    height: 180px;
+    margin-left: 15px;
+    margin-bottom: 10rpx;
+  }
 
-.bodaydata {
-  margin-top: 25px;
-}
-.userdatadiv {
-  display: inline;
-  margin-left: 15px;
-}
-.userdatatitle {
-  font-size: 30px;
-}
+  .logo {
+    width: 70px;
+    height: 70px;
+  }
 
-.keyname {
-  font-size: 20px;
-}
-.keyvalue {
-  font-size: 25px;
-  color: red;
-  font-weight: 600;
-}
-.handle-box {
-  margin-bottom: 20px;
-}
+  .bodaydata {
+    margin-top: 25px;
+  }
 
-.handle-select {
-  width: 120px;
-}
+  .userdatadiv {
+    display: inline;
+    margin-left: 15px;
+  }
 
-.handle-input {
-  width: 300px;
-  display: inline-block;
-}
-.del-dialog-cnt {
-  font-size: 16px;
-  text-align: center;
-}
-.table {
-  width: 100%;
-  font-size: 14px;
-}
-.red {
-  color: #ff0000;
-}
+  .userdatatitle {
+    font-size: 30px;
+  }
+
+  .keyname {
+    font-size: 20px;
+  }
+
+  .keyvalue {
+    font-size: 25px;
+    color: red;
+    font-weight: 600;
+  }
+
+  .handle-box {
+    margin-bottom: 20px;
+  }
+
+  .handle-select {
+    width: 120px;
+  }
+
+  .handle-input {
+    width: 300px;
+    display: inline-block;
+  }
+
+  .del-dialog-cnt {
+    font-size: 16px;
+    text-align: center;
+  }
+
+  .table {
+    width: 100%;
+    font-size: 14px;
+  }
+
+  .red {
+    color: #ff0000;
+  }
 </style>
